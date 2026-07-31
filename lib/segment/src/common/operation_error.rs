e84c780063e77ec8832c9720cbae36d73e47ffe2
Luis Cossío <luis.cossio@outlook.com>
2026-07-31 13:36:27 -0400

    add `UniversalIoError::UnchangedOpen`

--- lib/segment/src/common/operation_error.rs
+++ lib/segment/src/common/operation_error.rs
@@ -230,6 +230,7 @@ impl From<UniversalIoError> for OperationError {
             | UniversalIoError::OutOfBounds { .. }
             | UniversalIoError::InvalidFileIndex { .. }
             | UniversalIoError::Uninitialized { .. }
+            | UniversalIoError::UnchangedOpen { .. }
             | UniversalIoError::QueueIsFull
             | UniversalIoError::AppendOffsetConflict { .. }
             | UniversalIoError::S3(_)
@@ -354,6 +355,7 @@ impl From<BlobstoreError> for OperationError {
                 | UniversalIoError::OutOfBounds { .. }
                 | UniversalIoError::InvalidFileIndex { .. }
                 | UniversalIoError::Uninitialized { .. }
+                | UniversalIoError::UnchangedOpen { .. }
                 | UniversalIoError::QueueIsFull
                 | UniversalIoError::AppendOffsetConflict { .. }
                 | UniversalIoError::S3(_)
